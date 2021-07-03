daf8c7c7bc89355443bef5c295b2e37788f2d4dd
KhayrulloevDD <dodullo.khayrulloev@gmail.com>
2021-07-03 14:48:29 +0300

    Liskov substitution

--- solid.py
+++ solid.py
@@ -21,29 +21,37 @@ class Order:
 
 class PaymentProcessor(ABC):
     @abstractmethod
-    def pay(self, order, security_code):
+    def pay(self, order):
         pass
 
 
 class DebitPaymentProcessor(PaymentProcessor):
+    def __init__(self, security_code):
+        self.security_code = security_code
 
-    def pay(self, order, security_code):
+    def pay(self, order):
         print("Processing debit payment type")
-        print(f"Verifying security code: {security_code}")
+        print(f"Verifying security code: {self.security_code}")
         order.status = "paid"
 
 
 class CreditPaymentProcessor(PaymentProcessor):
-    def pay(self, order, security_code):
+    def __init__(self, security_code):
+        self.security_code = security_code
+
+    def pay(self, order):
         print("Processing credit payment type")
-        print(f"Verifying security code: {security_code}")
+        print(f"Verifying security code: {self.security_code}")
         order.status = "paid"
 
 
 class PaypalPaymentProcessor(PaymentProcessor):
-    def pay(self, order, security_code):
+    def __init__(self, email_address):
+        self.email_address = email_address
+
+    def pay(self, order):
         print("Processing paypal payment type")
-        print(f"Verifying security code: {security_code}")
+        print(f"Verifying email address: {self.email_address}")
         order.status = "paid"
 
 
@@ -53,5 +61,5 @@ order.add_item("SSD", 1, 150)
 order.add_item("USB Cable", 2, 5)
 
 print(order.total_price())
-processor = PaypalPaymentProcessor()
-processor.pay(order, "0372846")
+processor = PaypalPaymentProcessor("dd@k.com")
+processor.pay(order)
